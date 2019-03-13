293020258547d353b8982bccb4be12451beae72b
Xunjin <xunjin.coder@gmail.com>
2019-03-13 09:39:47 -0300

    Create the Admin module, from now on it's security and deployment

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -2,16 +2,29 @@
 using Microsoft.AspNetCore.Http;
 using SportsStore.Models;
 using System.Linq;
+using SportsStore.Models.ViewModels;
+using SportsStore.Infrastructure;
 
 namespace SportsStore.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
         }
 
         public RedirectToActionResult AddtoCart(int productId, string returnUrl)
@@ -21,9 +34,7 @@ namespace SportsStore.Controllers
 
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
@@ -36,22 +47,9 @@ namespace SportsStore.Controllers
 
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }
-
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
